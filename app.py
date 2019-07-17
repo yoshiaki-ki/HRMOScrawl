65ed11c55957b69d7442db897dde3241d6826c79
yoshiaki-ki <wp.yk68td@gmail.com>
2019-07-17 19:01:50 +0900

    set framework

--- app.py
+++ app.py
@@ -8,6 +8,66 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+
+@app.route("/api/get/all", methods=["GET", "POST"])
+def get_all_jobs():
+    """
+    企業、案件を全て取得する
+    :param:
+    id : str
+        ログイン用ID
+    password : str
+        ログイン用パスワード
+
+    :return:
+    job_list : list
+    """
+    if request.method == "GET":
+        pass
+    else:
+        try:
+            pass
+
+        except:
+            pass
+
+
+@app.route("/api/get/companies", methods=["GET", "POST"])
+def get_companies():
+    """
+    企業名を取得する
+    :param:
+    id : str
+        ログイン用ID
+    password : str
+        ログイン用パスワード
+
+    :return:
+    company_list : list
+
+    """
+    pass
+
+
+#
+@app.route("/api/get/jobs", methods=["GET", "POST"])
+def get_jobs():
+    """
+    案件を取得する
+    :param:
+    id : str
+        ログイン用ID
+    password : str
+        ログイン用パスワード
+    company_num : int
+        企業番号
+
+    :return:
+    job_list : list
+    """
+    pass
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=80)
